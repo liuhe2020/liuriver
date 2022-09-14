fa288095e1c177c3475025bbb90ed490ecf743e0
Liu He <lhe19@yahoo.co.uk>
2022-09-14 19:28:54 +0100

    add js code auto update year in footer

--- js/index.js
+++ js/index.js
@@ -1,12 +1,20 @@
-"use strict";
+'use strict';
 
 // Navbar
-const navTogglerBtn = document.querySelector(".toggler");
-const sidebar = document.querySelector(".sidebar");
+const navTogglerBtn = document.querySelector('.toggler');
+const sidebar = document.querySelector('.sidebar');
 
-navTogglerBtn.addEventListener("click", sideBarTogglerBtn);
+navTogglerBtn.addEventListener('click', sideBarTogglerBtn);
 
 function sideBarTogglerBtn() {
-  sidebar.classList.toggle("open");
-  navTogglerBtn.classList.toggle("open");
+  sidebar.classList.toggle('open');
+  navTogglerBtn.classList.toggle('open');
 }
+
+// Footer
+const copyright = document.querySelector('.copyright');
+
+const today = new Date();
+const year = today.getFullYear();
+
+copyright.innerText = `© ${year} LIU RIVER PHOTOGRAPHY`;
